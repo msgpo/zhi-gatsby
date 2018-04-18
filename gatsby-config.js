a64573120e3a6bb023eeb347cccac0dced91a888
yongzhihuang <yongzhihuang@yahoo.com>
2018-04-17 21:50:07 -0400

    Added gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,20 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: false
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
     'gatsby-plugin-typography',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass'
