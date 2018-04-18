f272285a40219f99f3a6402d2c0b2f29f7efb689
yongzhihuang <yongzhihuang@yahoo.com>
2018-04-17 21:46:17 -0400

    first

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-typography',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass'
+  ],
+}
